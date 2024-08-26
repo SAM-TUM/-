b4d7c5e06c40f500b2578fabed41a6247a964709
wkbys01 <wakabayashi.m.24kdgn@gmail.com>
2024-08-26 15:07:55 +0900

    Update advice.py

--- advice.py
+++ advice.py
@@ -1,11 +1,14 @@
 #はい or いいえ
-x = "入力"
+x = input()
 question = "質問のリスト"
 
+#質問が無くなるまで繰り返し
 while question > 0:
+    
     if x == "はい":
       print("質問に対応したアドバイス")
     else:
       pass
+        
 elif question == 0:
     print("完了ボタン")
