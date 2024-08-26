64909d78b77e284a241fb9bb7412d5876277a43c
t-t328 <toyama.t.24kdgn@gmail.com>
2024-08-26 14:57:20 +0900

    Update contents.py

--- contents.py
+++ contents.py
@@ -1,1 +1,23 @@
-a
+#アプリ内容 
+#質問分野
+
+#csvでデータ取得　仮でinput
+gender =input()
+
+#question変数に質問をcsvからリスト形式で渡す
+if gender =="male":
+  #男性用処理
+  questionLi =input("男性")
+elif gender =="female":
+  #女性用処理
+  questionLi =input("女性")
+
+#HTMLでデザイン
+for index,question in enumerate(questionLi):
+  print(question)
+  
+  #アドバイス出力処理
+
+  #入力受付
+  button =input("Y or N")
+
