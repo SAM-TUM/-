3ffa1716c699ea8ae4301985eb52feee7c06dbbf
nero05251014 <122855481+nero05251014@users.noreply.github.com>
2024-08-31 09:18:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,18 @@ import os
 
 app = Flask(__name__)
 
-# 現在のディレクトリパスを取得して、SQLiteデータベースをそのディレクトリに保存
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'survey.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# アンケート結果を保存するテーブルのモデルを定義
 class SurveyResponse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     feedback = db.Column(db.Text, nullable=False)
 
-# テーブルを作成
 with app.app_context():
     db.create_all()
 
@@ -32,7 +29,6 @@ def submit():
     age = request.form['age']
     feedback = request.form['feedback']
     
-    # データベースに保存
     response = SurveyResponse(name=name, age=age, feedback=feedback)
     db.session.add(response)
     db.session.commit()
@@ -45,7 +41,6 @@ def thank_you():
 
 @app.route('/results')
 def results():
-    # データベースから全てのアンケート結果を取得
     responses = SurveyResponse.query.all()
     return render_template('results.html', responses=responses)
 
